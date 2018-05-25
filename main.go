24df95f48325d78798664c5ae1380fc52fe83f87
Mathieu Champlon <mathieu.champlon@docker.com>
2018-05-25 10:20:54 +0200

    Centralize error handling and exit codes

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"io"
 	"io/ioutil"
 	"os"
-	"path"
 	"path/filepath"
 
 	"github.com/mitchellh/go-homedir"
@@ -14,9 +13,10 @@ import (
 func main() {
 	args := os.Args[1:]
 	action := args[0]
-
+	var err error
 	switch action {
 	case "help":
+	case "--help":
 		printUsage()
 	case "home":
 		home, err := homedir.Dir()
@@ -26,124 +26,23 @@ func main() {
 		}
 		fmt.Print(home)
 	case "rm":
-		checkArgumentsOrQuit(args, 2)
-		remove(args[1])
+		checkArguments(args, 2)
+		err = remove(args[1])
 	case "cp":
-		checkArgumentsOrQuit(args, 3)
-		copy(args[1], args[2])
+		checkArguments(args, 3)
+		err = copy(args[1], args[2])
 	default:
 		fmt.Printf("I don't know what %v means\n", action)
 		printUsage()
 		os.Exit(-3)
 	}
-}
-
-func remove(source string) {
-	fmt.Println("Removing", source)
-	_, err := os.Stat(source)
-	if os.IsNotExist(err) {
-		fmt.Printf("Source [%v] does not exist, doing nothing\n", source)
-		return
-	}
-	err = os.RemoveAll(source)
 	if err != nil {
 		fmt.Println(err)
+		os.Exit(-1)
 	}
 }
 
-func copy(source, destination string) {
-	sourceFile, err := os.Stat(source)
-	if os.IsNotExist(err) {
-		fmt.Printf("Source [%v] does not exist\n", source)
-		os.Exit(-2)
-	}
-	if sourceFile.Mode().IsDir() {
-		err := copyDirectory(source, destination)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(-3)
-		}
-	} else {
-		destinationPath := filepath.Dir(destination)
-		_, err := os.Stat(destinationPath)
-		if os.IsNotExist(err) {
-			sourcePath := filepath.Dir(source)
-			sourceDir, err := os.Stat(sourcePath)
-			if err != nil {
-				fmt.Println(err)
-				os.Exit(-3)
-			}
-			os.MkdirAll(destinationPath, sourceDir.Mode())
-		}
-		err = copyFile(source, destination)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(-3)
-		}
-	}
-}
-
-func copyFile(src, dst string) error {
-	var err error
-	var sourceFD *os.File
-	var destinationFD *os.File
-	var sourceFileInfo os.FileInfo
-
-	fmt.Printf("Copying file [%v] to [%v]\n", src, dst)
-	if sourceFD, err = os.Open(src); err != nil {
-		return err
-	}
-	defer sourceFD.Close()
-
-	if destinationFD, err = os.Create(dst); err != nil {
-		return err
-	}
-	defer destinationFD.Close()
-
-	if _, err = io.Copy(destinationFD, sourceFD); err != nil {
-		return err
-	}
-	if sourceFileInfo, err = os.Stat(src); err != nil {
-		return err
-	}
-	return os.Chmod(dst, sourceFileInfo.Mode())
-}
-
-func copyDirectory(src string, dst string) error {
-	var err error
-	var fds []os.FileInfo
-	var srcinfo os.FileInfo
-
-	fmt.Printf("Copying dir [%v] to [%v]\n", src, dst)
-	if srcinfo, err = os.Stat(src); err != nil {
-		return err
-	}
-
-	if err = os.MkdirAll(dst, srcinfo.Mode()); err != nil {
-		return err
-	}
-
-	if fds, err = ioutil.ReadDir(src); err != nil {
-		return err
-	}
-	for _, fd := range fds {
-		srcfp := path.Join(src, fd.Name())
-		dstfp := path.Join(dst, fd.Name())
-
-		if fd.IsDir() {
-			if err = copyDirectory(srcfp, dstfp); err != nil {
-				fmt.Println(err)
-			}
-		} else {
-			if err = copyFile(srcfp, dstfp); err != nil {
-				fmt.Println(err)
-			}
-		}
-	}
-	return nil
-}
-
-func checkArgumentsOrQuit(args []string, max int) {
+func checkArguments(args []string, max int) {
 	if len(args) < max {
 		fmt.Println("Not enough arguments, I'm the stupid one, you fix it")
 		printUsage()
@@ -156,4 +55,85 @@ func printUsage() {
 	fmt.Println("* stupid home")
 	fmt.Println("* stupid cp SRC DST")
 	fmt.Println("* stupid rm SRC")
+}
+
+func remove(source string) error {
+	fmt.Println("Removing", source)
+	_, err := os.Stat(source)
+	if os.IsNotExist(err) {
+		fmt.Printf("Source [%v] does not exist, doing nothing\n", source)
+		return nil
+	}
+	return os.RemoveAll(source)
+}
+
+func copy(source, destination string) error {
+	sourceInfo, err := os.Stat(source)
+	if os.IsNotExist(err) {
+		return fmt.Errorf("Source [%v] does not exist", source)
+	}
+	if err != nil {
+		return err
+	}
+	if sourceInfo.Mode().IsDir() {
+		return copyDirectory(source, destination)
+	}
+	sourceDirInfo, err := os.Stat(filepath.Dir(source))
+	if err != nil {
+		return err
+	}
+	err = os.MkdirAll(filepath.Dir(destination), sourceDirInfo.Mode())
+	if err != nil {
+		return err
+	}
+	return copyFile(source, destination)
+}
+
+func copyFile(src, dst string) error {
+	fmt.Printf("Copying file [%v] to [%v]\n", src, dst)
+	source, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer source.Close()
+	destination, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer destination.Close()
+	if _, err = io.Copy(destination, source); err != nil {
+		return err
+	}
+	info, err := os.Stat(src)
+	if err != nil {
+		return err
+	}
+	return os.Chmod(dst, info.Mode())
+}
+
+func copyDirectory(src string, dst string) error {
+	fmt.Printf("Copying dir [%v] to [%v]\n", src, dst)
+	info, err := os.Stat(src)
+	if err != nil {
+		return err
+	}
+	if err = os.MkdirAll(dst, info.Mode()); err != nil {
+		return err
+	}
+	infos, err := ioutil.ReadDir(src)
+	if err != nil {
+		return err
+	}
+	for _, info := range infos {
+		srcfp := filepath.Join(src, info.Name())
+		dstfp := filepath.Join(dst, info.Name())
+		if info.IsDir() {
+			if err = copyDirectory(srcfp, dstfp); err != nil {
+				return err
+			}
+		} else if err = copyFile(srcfp, dstfp); err != nil {
+			return err
+		}
+	}
+	return nil
 }
